edcf77f8ac283f9822f46415bf842960cc1a1a6d
Rodrigo Soares <diihhsoares2@gmail.com>
2023-08-09 18:34:59 -0300

    adicionando projeto

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+      <h1>Calculadora Vue</h1>
+      <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+
+export default {
+    components: { Calculator }
+}
+</script>
+
+<style>
+
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+    font-family: "RobotoMono", monospace;
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(319deg, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+
+h1 {
+    font-size: 40px;
+    text-align: center;
+    color: #fff;
+    position: relative;
+    top: -60px;
+}
+
+</style>
